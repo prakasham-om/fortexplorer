85d08d26d96c9f001366d2408b2e985daf3ea9db
Prakash <prakashchandrasahoo8991@gmail.com>
2023-08-10 12:02:26 +0530

    hello

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -162,7 +162,7 @@ alert:'0'
                     </div>
                 </div>
                 <div className="d-flex justify-content-between mt-5">
-                       <div className='d-none d-lg-block'>©️ 2023 Forta Foundation</div>
+                       <div className='d-none d-md-block'>©️ 2023 Forta Foundation</div>
                        <div className='text-center'>Discord-Terms of Service-Privacy Policy</div>
                 </div>
                 </div>
